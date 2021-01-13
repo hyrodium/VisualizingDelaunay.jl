d0d30a53ea6affafba05e547cf2f33c5654ec976
hyrodium <hyrodium@gmail.com>
2021-01-14 01:02:24 +0900

    update

--- VisualizingDelaunay.jl
+++ VisualizingDelaunay.jl
@@ -26,6 +26,22 @@ struct Face
     end
 end
 
+struct Mesh2D
+    points::Vector{Point2D}
+    vertices::Vector{Vertex}
+    edges::Vector{Edge}
+    faces::Vector{Face}
+end
+
+function Mesh2D(min_x, max_x, min_y, max_y)
+    ps = [Point2D(min_x,min_y), Point2D(max_x,min_y), Point2D(min_x,max_y), Point2D(max_x,max_y)]
+    vs = [Vertex(i) for i in 1:4]
+    es = [Edge(1,2),Edge(1,3),Edge(2,4),Edge(3,4)]
+    fs = [Face(1,2,3),Face(2,3,4)]
+    return Mesh2D(ps,vs,es,fs)
+end
+
+
 Base.:(==)(v1::Vertex, v2::Vertex) = v1.i1 == v2.i1
 Base.:(==)(e1::Edge, e2::Edge) = (e1.i1, e1.i2) == (e2.i1, e2.i2)
 Base.:(==)(f1::Face, f2::Face) = (f1.i1, f1.i2, f1.i3) == (f2.i1, f2.i2, f2.i3)
@@ -55,7 +71,12 @@ function indexedcolor(i)
     RGB(rand(m),rand(m),rand(m))
 end
 
-function draw(ps::Vector{Point2D}, vs::Vector{Vertex} ,es::Vector{Edge} ,fs::Vector{Face}; withcircle=true, name=nothing)
+function draw(mesh::Mesh2D; withcircle=true, name=nothing)
+    ps = mesh.points
+    vs = mesh.vertices
+    es = mesh.edges
+    fs = mesh.faces
+
     @svg begin
         d = 1000
         if isnothing(name)
@@ -130,6 +151,9 @@ function _isvalid(e::Edge, fs, ps)
     elseif length(f_touch) == 2
         v2, v4 = _vertices(e)
         vs_tmp = unique(vcat(_vertices.(f_touch)...))
+        println("f_touch: ", f_touch)
+        println("vs_tmp: ", vs_tmp)
+        println("v2, v4: ", (v2, v4))
         v1, v3 = setdiff(vs_tmp, [v2, v4])
         p1, p2, p3, p4 = ps[v1.i1], ps[v2.i1], ps[v3.i1], ps[v4.i1]
         α = acos(clamp(dot(p2-p1, p4-p1)/norm(p2-p1)/norm(p4-p1), -1, 1))
@@ -155,7 +179,15 @@ function _flip!(e::Edge, es, fs, es_vague)
     setdiff!(es_vague, [e])
     push!(es, Edge(i1,i3))
     setdiff!(fs, f_touch)
+    if fs ≠ unique(fs)
+        error("afa")
+    end
+
     push!(fs, Face(i1,i2,i3), Face(i1,i3,i4))
+    if fs ≠ unique(fs)
+        println([i1,i2,i3,i4])
+        error("afa2")
+    end
     push!(es_vague, Edge(i1, i2), Edge(i2, i3), Edge(i3, i4), Edge(i1, i4))
     return
 end
@@ -206,49 +238,49 @@ function _face_include_p(p,ps,fs)
     error("no triangle")
 end
 
-function main()
-    N = 250
-    d = 1000
-    _ps = [Point2D(0,0), Point2D(1000,0), Point2D(0,1000), Point2D(1000,1000)]
-    _vs = [Vertex(i) for i in 1:N]
-    _es = [Edge(1,2),Edge(1,3),Edge(2,4),Edge(3,4)]
+function newpoint!(mesh::Mesh2D, p::Point2D)
+    i = length(mesh.points) + 1
+    push!(mesh.points, p)
+    push!(mesh.vertices, Vertex(i))
 
-    push!(_ps,Point2D(rand(1:d), rand(1:d)))
-    append!(_es, [Edge(5,1),Edge(5,2),Edge(5,3),Edge(5,4)])
-    _fs = [Face(1,2,5),Face(1,3,5),Face(2,4,5),Face(3,4,5)]
+    f_l = length(mesh.faces)
+    _f = _face_include_p(mesh.points[end], mesh.points, mesh.faces)
 
-    for i in 6:N
-        new_point = Point2D(rand(1:d), rand(1:d))
-        _ps = push!(_ps, new_point)
+    i1 = _f.i1
+    i2 = _f.i2
+    i3 = _f.i3
+    _es_vague = _edges(_f, mesh.edges)
+    push!(mesh.edges,Edge(i1,i),Edge(i2,i),Edge(i3,i))
+    push!(mesh.faces,Face(i1,i2,i),Face(i2,i3,i),Face(i1,i3,i))
+    setdiff!(mesh.faces,[_f])
 
-        f_l = length(_fs)
-        _f = _face_include_p(_ps[end], _ps, _fs)
+    while true
+        if isempty(_es_vague)
+            break
+        end
 
-        i1 = _f.i1
-        i2 = _f.i2
-        i3 = _f.i3
-        _es_vague = _edges(_f, _es)
-        push!(_es,Edge(i1,i),Edge(i2,i),Edge(i3,i))
-        push!(_fs,Face(i1,i2,i),Face(i2,i3,i),Face(i1,i3,i))
-        setdiff!(_fs,[_f])
-
-        while true
-            if isempty(_es_vague)
-                break
-            end
-
-            l = length(_es_vague)
-            e = _es_vague[end]
-            if _isvalid(e, _fs, _ps)
-                pop!(_es_vague)
-            else
-                _flip!(e, _es, _fs, _es_vague)
-            end
+        l = length(_es_vague)
+        e = _es_vague[end]
+        if _isvalid(e, mesh.faces, mesh.points)
+            pop!(_es_vague)
+        else
+            _flip!(e, mesh.edges, mesh.faces, _es_vague)
         end
     end
+end
 
-    draw(_ps, _vs, _es, _fs, name="save.png")
+function main()
+    N = 100
+    d = 1000
+    mesh = Mesh2D(0,d,0,d)
+
+    for i in 5:N
+        new_point = Point2D(rand(1:d), rand(1:d))
+        newpoint!(mesh, new_point)
+        draw(mesh, name="save$(lpad(i,3,'0')).png")
+    end
+
+    draw(mesh, name="save.png")
 end
 
 main()
-
